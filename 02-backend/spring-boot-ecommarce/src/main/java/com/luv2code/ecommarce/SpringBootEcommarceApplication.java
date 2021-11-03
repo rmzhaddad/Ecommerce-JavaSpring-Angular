0edca9f31f724f88f6f16bd6d7b635d56ef3788e
Ramzi <ramzi.haddad@ontariotechu.net>
2021-11-03 18:21:11 -0400

    Release 1.0

---
+++ 02-backend/spring-boot-ecommarce/src/main/java/com/luv2code/ecommarce/SpringBootEcommarceApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.ecommarce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcommarceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcommarceApplication.class, args);
+	}
+
+}
---
+++ 02-backend/spring-boot-ecommarce/src/test/java/com/luv2code/ecommarce/SpringBootEcommarceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.ecommarce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommarceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
