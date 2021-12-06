7921c725d07e784b689e55a7aad7a1baaf5e92c6
Ramzi <ramzi.haddad@ontariotechu.net>
2021-12-06 14:25:38 -0500

    Release 3.0: added okta registration  functionlity to frontend

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc:{
+        clientId:'0oa2z458mwLQja2Tb5d7',
+        issuer: 'https://dev-02906113.okta.com/oauth2/default',
+        redirectUri:'http://localhost:4200/login/callback',
+        scopes:['openid','profile','email']
+    }
+}
